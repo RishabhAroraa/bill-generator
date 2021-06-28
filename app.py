f52bcb442632446fd6d88edfe27b3b82ecb12617
rishabharoraa <rishabharora.ns@gmail.com>
2021-06-28 15:15:11 +0530

    added fields

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+# author: rishabharoraa.github.io
 from tkinter import *
 from tkinter import ttk
 
+# basic configuration
 window = Tk()
-window.title("Report Card Generator")
+window.title("Bill generator")
+window.geometry("480x640")
+#window.configure(background="gray")
 
-window.geometry("480x720")
-window.configure(background="gray")
-ttk.Button(window, text="Hello").grid()
+product_ID = Label(window, text="Product ID: ").grid(column=0, row=0)
+product_name = Label(window, text="Product Name: ").grid(column=0, row=1)
+product_units = Label(window, text="Units: " ).grid(column=0, row=2)
+product_price = Label(window, text="Amount: ").grid(column=0,row=3)
+
+product_ID_entry = Entry(window).grid(column=1, row=0)
+product_name_entry = Entry(window).grid(column=1, row=1)
+product_units_entry = Entry(window).grid(column=1, row=2)
+product_price_entry = Entry(window).grid(column=1, row=3)
+
+add_item_button = ttk.Button(window, text="Add Item").grid(row=4,column=0)
 
 window.mainloop()
