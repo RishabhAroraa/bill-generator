3e16d4d4a55d937edb45601c1d42bdf8da6e5036
rishabharoraa <rishabharora.ns@gmail.com>
2021-06-28 14:51:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+window.title("Report Card Generator")
+
+window.geometry("480x720")
+window.configure(background="gray")
+ttk.Button(window, text="Hello").grid()
+
+window.mainloop()
