27b246adf1455fa73fd0255994263796d389790c
Rishabh Arora <rishabharora.ns@gmail.com>
2021-06-29 14:48:48 +0530

    Edit app.py

--- app.py
+++ app.py
@@ -4,25 +4,38 @@ from tkinter import ttk
 
 # basic configuration
 window = Tk()
-window.title("Bill generator")
+window.title("Employee Report")
 window.geometry("480x640")
 
+# window.background("#282828")
 
-# labels and entries
-product_ID = Label(window, text="Product ID: ").grid(column=0, row=0)
-product_name = Label(window, text="Product Name: ").grid(column=0, row=1)
-product_units = Label(window, text="Units: " ).grid(column=0, row=2)
-product_price = Label(window, text="Amount: ").grid(column=0,row=3)
+# heading
+heading = Label(window, text="Generate Weekly Report", font = ('',15,'bold'), pady=20).pack()
 
-product_ID_entry = Entry(window).grid(column=1, row=0)
-product_name_entry = Entry(window).grid(column=1, row=1)
-product_units_entry = Entry(window).grid(column=1, row=2)
-product_price_entry = Entry(window).grid(column=1, row=3)
+# form
+employeeID = Label(window, text="Employee ID").pack()
+employeeIDEntry = Entry(window).pack()
+
+employeeName = Label(window, text="Employee Name").pack()
+employeeNameEntry = Entry(window).pack()
+
+employeeDepartment = Label(window, text="Department").pack()
+employeeDepartmentEntry = Entry(window).pack()
+
+completedTasks = Label(window, text="Completed Tasks").pack()
+completedTasksEntry = Text(window, height=7, width=50).pack()
+
+ongoingTasks = Label(window, text="Ongoing Tasks").pack()
+ongoingTasksEntry = Text(window, height=7, width=50).pack()
+
+nextWeekTasks = Label(window, text="Tasks for next week").pack()
+nextWeekTasksEntry = Text(window, height=7, width=50).pack()
+
+submitItem = Button(window, text="Submit").pack()
 
 # TODO :
 # make a new Bill Object
 # make a new Item Object when the button is clicked
 
-add_item_button = ttk.Button(window, text="Add Item", command='add_item_button_clicked').grid(row=4,column=0)
 
 window.mainloop()
